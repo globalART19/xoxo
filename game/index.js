1a24216cf458c521adc3b04c0f74c597c2ccb212
AndrewTrahan <globalART19@gmail.com>
2018-05-16 11:17:49 -0500

    I win game 1!!!

--- game/index.js
+++ game/index.js
@@ -1,6 +1,24 @@
-import {Map} from 'immutable'
+import { Map } from 'immutable'
 
-export default function reducer(state, action) {
-  // TODO
+//Action Types
+const MOVE = 'MOVE'
+
+const initialState = {
+  turn: 'X',
+  board: Map()
+}
+
+// Action Creators
+export const move = (turn, position) => {
+  return { type: MOVE, position }
+}
+
+export default function reducer(state = initialState, action) {
+  if (action.type === MOVE) {
+    return {
+      turn: (state.turn === 'X' ? 'O' : 'X'),
+      board: state.board.setIn(action.position, state.turn)
+    }
+  }
   return state
 }
