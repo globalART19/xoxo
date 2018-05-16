707d689677575b50ef0ee157f238e72a507f1030
AndrewTrahan <globalART19@gmail.com>
2018-05-16 16:15:37 -0500

    we broke the ai, but its almost close maybe?

--- game/ai.js
+++ game/ai.js
@@ -9,7 +9,7 @@ export const moves = game => {
   const possibleMoves = []
   for (let row = 0; row < 3; row++) {
     for (let col = 0; col < 3; col++) {
-      if (!badMove(game, move('O', [row, col]))) possibleMoves.push([row, col])
+      if (!badMove(game, move(game.turn, [row, col]))) possibleMoves.push(move(game.turn, [row, col]))
     }
   }
   return possibleMoves
@@ -23,14 +23,19 @@ export const moves = game => {
  * Scores will be numbers from -1 to +1. 1 is a winning state, -1
  * is state from which we can only lose.
  */
-const score = (game, position) => {
+const score = (game, moveObj) => {
   //winning condition
   //if 2 in a row && 3rd empty, win
-  if (winner(boardReducer(game.board, move('O', position))) === 'O') return 1
-  // losing condition
-  // if opponent 2 in a row && third empty
-  if (winner(boardReducer(game.board, move('X', position))) === 'X') return .9
-  return 0
+  // if (winner(boardReducer(game.board, move('O', position))) === 'O') return 1
+  // // losing condition
+  // // if opponent 2 in a row && third empty
+  // if (winner(boardReducer(game.board, move('X', position))) === 'X') return .99
+
+  const future = reducer(game, moveObj)
+  if (future.winner === moveObj.player) return 1
+  if (future.winner === 'draw') return 0
+  if (!future.winner) return -Math.max(...moves(future).map(move => score(future, moveObj)))
+  return -1
 }
 
 /**
@@ -40,10 +45,10 @@ const score = (game, position) => {
  */
 export default state => {
   const possibleMoves = moves(state)
-  const scoreArray = possibleMoves.map(position => {
-    return score(state, position)
+  const scoreArray = possibleMoves.map(move => {
+    return score(state, move)
   })
   const sortedArray = [...scoreArray].sort((a, b) => { return b - a })
   const moveIndex = scoreArray.indexOf(sortedArray[0])
-  return move('O', possibleMoves[moveIndex])
+  return possibleMoves[moveIndex]
 }
