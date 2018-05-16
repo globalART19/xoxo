8f88b9574ab50062950d7528367404c7f1aa406d
chawes <hawes.conor@gmail.com>
2018-05-16 12:03:06 -0500

    Implemented winner function

--- game/index.js
+++ game/index.js
@@ -8,6 +8,52 @@ const initialState = {
   board: Map()
 }
 
+const streak = (board, firstCoord, ...remainingCoords) => {
+  const firstEntry = board.getIn(firstCoord);
+  if (firstEntry === undefined) return null;
+
+  const count = remainingCoords.reduce((acc, coord) => {
+    if (firstEntry === board.getIn(coord)) {
+      return acc + 1
+    }
+  }, 0)
+
+  return count === 2 ? firstEntry : null;
+}
+
+const winner = (board) => {
+  let winner = null;
+
+  for (let row = 0; row < 3; row++) {
+    winner = streak(board, [row, 0], [row, 1], [row, 2])
+    if (winner) return winner;
+  }
+
+  for (let col = 0; col < 3; col++) {
+    winner = streak(board, [0, col], [1, col], [2, col])
+    if (winner) return winner;
+  }
+
+  winner = streak(board, [0, 0], [1, 1], [2, 2])
+  if (winner) return winner;
+
+  winner = streak(board, [0, 2], [1, 1], [2, 0])
+
+  if (winner) {
+    return winner;
+  } else {
+    for (let i = 0; i < 3; i++) {
+      for (let j=0; j < 3; j++) {
+        if (!Map.getIn([i, j])) return 'ongoing';
+      }
+    }
+  }
+
+  return 'draw';
+}
+
+
+
 // Action Creators
 export const move = (turn, position) => {
   return { type: MOVE, position }
